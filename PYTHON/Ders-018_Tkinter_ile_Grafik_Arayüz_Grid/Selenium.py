ac11716fc13ffaa2b77c52f7b8d3bb75a6cafc49
Mehmet Demir <mail.mehmetdemir@gmail.com>
2020-10-08 00:52:30 +0300

    Selenium güncellendi.

--- PYTHON/Ders-018_Tkinter_ile_Grafik_Arayüz_Grid/Selenium.py
+++ PYTHON/Ders-018_Tkinter_ile_Grafik_Arayüz_Grid/Selenium.py
@@ -31,3 +31,38 @@ if (__name__ == '__main__'):
 
 
 
+from Doviz import *
+
+from selenium import webdriver
+import time
+
+'''
+altin,dolar,euro = KurGetir()
+
+print("Altın :",altin)
+print("Dolar :",dolar)
+print("Euro :",euro)
+'''
+
+# Youtube'da arama yapma
+chrome = "C:\\selenium\\chromedriver.exe"
+browser = webdriver.Chrome(executable_path=chrome)
+browser.get("https://www.youtube.com")
+time.sleep(2)
+arama_kutusu = browser.find_element_by_name('search_query')
+#arama_kutusu.send_keys("Adam Savage's Spot Robot Rickshaw Carriage!")
+arama_kutusu.send_keys("Youtube Originals AI")
+
+time.sleep(1)
+arama_butonu = browser.find_element_by_id('search-icon-legacy')
+arama_butonu.click()
+time.sleep(2)
+
+# aramada çıkan ilk elemana tıklıyoruz.
+ilk_video = browser.find_element_by_xpath('//*[@id="contents"]/ytd-video-renderer[1]')
+ilk_video.click()
+
+
+
+
+
